9399bdd4dcb97533f8e4c6c229877dd1538b9fd8
Gijs van Til <gijsvantil@gmail.com>
2016-05-22 21:58:53 +0200

    Created a POST to handle form input and installed mongodb

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 console.log('May Node be with you')
 
 const express = require('express')
+const bodyParser = require ('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 app.get ('/', (request, response)=>{
 	response.sendFile (__dirname + '/index.html')
 });
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
 
 
 app.listen(3000, function(){
