1ef544ee3a479a8b3703f4a3328741909adf9a45
Gijs van Til <gijsvantil@gmail.com>
2016-05-22 17:06:15 +0200

    created first GET

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ console.log('May Node be with you')
 const express = require('express')
 const app = express()
 
+app.get ('/', (request, response)=>{
+	response.sendfile ()
+});
 
 
 app.listen(3000, function(){
